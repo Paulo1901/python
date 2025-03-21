27416baa1925dea8301457703dbb93835c7b3bfc
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-21 08:41:47 -0300

    init types int and float

---
+++ Python/logica-programacao/aula004_int_float.py
@@ -0,0 +1,13 @@
+# Tipo int e float
+# int -> número inteiro
+# o tipo int representa qualquer número 
+# positivo ou negativo. int sem sinal é considerado
+
+# float -> número com ponto flutuante
+# o tipo de dado float representa qualquuer número
+# positivo ou negativo de ponto flutuante
+# float sem sinal é considerado positivo
+
+# A função type mostra o tipo que o Python
+# inferiu ao valor
+
