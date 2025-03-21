84e79d859d4f1309fbf15315f648f832c37598b0
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-21 08:32:38 -0300

    init type str

---
+++ Python/logica-programacao/aula003_string.py
@@ -0,0 +1,17 @@
+# Python = Linguagem de programação
+# Tipo de tipagem = Dinâmica / forte
+# Tipagem dinâmica é o Python ja sabe o tipo de informação que você esta passando
+# str -> string -> texto
+# String são textos que estão dentro de aspas
+
+# aspas simples - ''
+print('Paulo Carvalho')
+print("Paulo 'Carvalho'")
+
+# aspas duplas - ""
+print("Paulo Carvalho")
+print('Paulo "Carvalho"')
+
+# escapes - \n
+print('Paulo \"Carvalho')
+print(123) # o python ja sab que é um número inteiro
