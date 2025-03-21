819cf0fa0f984a15e35ff6860050a42787b72f55
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-21 01:40:12 -0300

    comando print

---
+++ Python/logica-programacao/aula002_funcao_print.py
@@ -0,0 +1,9 @@
+# Função print imprime algo na tela
+# cada print é exetutado um por linha
+# sep='' separador para os argumentos
+# \r\n - são operadores de escape para quebra de linha
+
+print(1, 2, 3, 4, sep='-', end='##')
+print(5, 6, 7, 8, sep="-")
+print(10, 12, sep='-')
+
