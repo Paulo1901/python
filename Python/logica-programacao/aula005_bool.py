1a890f5b4a24ef9bc40061b193f50e93a748781b
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-22 20:56:36 -0300

    type bool

---
+++ Python/logica-programacao/aula005_bool.py
@@ -0,0 +1,12 @@
+# Tipo de dados (booleano)
+# Ao questionar algo em um programa
+# só existe duas respostas possiveis
+# sim (True) ou nao (False)
+# Existe vários operadores para "questionar"
+# Dentre eles o ==, que é um operador lógico que 
+# questiona se um valor é igual a outro
+
+print(10 == 10) # sim => True (verdadeiro)
+print(10 == 11) # não => False (Falso)
+print(type(10 == 10)) # tipo de dados bool
+
