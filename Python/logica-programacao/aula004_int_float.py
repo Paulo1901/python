5ae3d1fef209b45c08287851104fdc2034896faf
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-21 23:03:34 -0300

    int and float

--- Python/logica-programacao/aula004_int_float.py
+++ Python/logica-programacao/aula004_int_float.py
@@ -3,11 +3,23 @@
 # o tipo int representa qualquer número 
 # positivo ou negativo. int sem sinal é considerado
 
+print(11) # int
+print(-11) # int
+print(0) # int
+
 # float -> número com ponto flutuante
 # o tipo de dado float representa qualquuer número
 # positivo ou negativo de ponto flutuante
 # float sem sinal é considerado positivo
 
+print(10.4) # float
+print(55.10) # float
+print(1.1, 10.11) # float
+print(0.0) # float
+
 # A função type mostra o tipo que o Python
 # inferiu ao valor
+print(type('11'))
+print(type(1))
+print(type(1.0))
 
