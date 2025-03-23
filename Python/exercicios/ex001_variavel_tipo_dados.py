679929950d42ad7ae0005cef892080b49e52e738
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-22 21:56:53 -0300

    first exercicio

---
+++ Python/exercicios/ex001_variavel_tipo_dados.py
@@ -0,0 +1,14 @@
+nome = 'Paulo'
+sobrenome = 'Carvalho'
+idade = 36
+ano_de_nascimento = 1989
+maior_de_idade = idade >= 18
+altura_metros = 1.80
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('idade:', idade)
+print('Ano de nascimento:', ano_de_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
+
