52e372492c55626e8bcd52e640b324626bd5a1bd
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-22 21:23:27 -0300

    coercion

---
+++ Python/logica-programacao/aula006_coercao_tipo.py
@@ -0,0 +1,12 @@
+# comversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+# tipo imutáveis e primitivos:
+# str, int, float, bool
+
+print(type(int('1')), int('1'))
+print(float('1') + 1)
+print(bool(''))
+print(str(11) + 'b')
+# print('a' + 'b') # concatena as duas strings
+
