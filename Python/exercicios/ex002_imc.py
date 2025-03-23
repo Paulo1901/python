086dcb8d86dc7d4c726f2412db81e026ccd22b04
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-22 23:20:57 -0300

    imc

---
+++ Python/exercicios/ex002_imc.py
@@ -0,0 +1,10 @@
+nome = 'Paulo'
+altura = 1.80
+peso = 98
+imc = peso / (altura * altura)
+
+# Paulo tem 1.80 de altura,
+# pesa 95 quilos e seu IMC é
+# 29.32032556544556]
+
+print(f'{nome} tem {altura} de altura, pesa {peso} quilos e seu IMC é {imc}')
