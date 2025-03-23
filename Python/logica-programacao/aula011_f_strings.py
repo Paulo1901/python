14d4b9795fbc5d4636e7c515ffa5afe046a5373a
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-23 01:06:13 -0300

    f-string

---
+++ Python/logica-programacao/aula011_f_strings.py
@@ -0,0 +1,12 @@
+# formatação de f-strings para melho legilibilidade do seu código
+
+nome_completo = 'Paulo Carvalho'
+idade = 36
+peso = 96
+saudacao = 'Seja ven-vindo!'
+formacao = 'Analise de desenvimento de sistema'
+
+
+print(f'{saudacao} meu nome é {nome_completo}')
+print(f'tenho {idade} anos e peso {peso} quilos')
+print(f'e sou formado em {formacao}')
