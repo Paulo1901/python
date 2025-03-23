7414b627ebf8451e17195566b9ccdf773c5e5abb
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-22 22:57:25 -0300

    concatenação

---
+++ Python/logica-programacao/aula009_concatenacao.py
@@ -0,0 +1,16 @@
+# Concatenação
+concatenacao = 'A' + 'B' + 'C' 
+print(concatenacao)
+
+# Repetição
+a_dez_vezes = 'A' * 10 
+print(a_dez_vezes)
+
+# Repetição
+tres_vezes_paulo = 3 * 'Paulo' 
+print(tres_vezes_paulo)
+
+# concatenação
+nome_concatenado = 'Paulo' + ' ' + 'Carvalho'
+print(nome_concatenado)
+
