ea3c3a79a1f7a8c75e8d3e49857dabbf604f46a7
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-22 22:46:49 -0300

    operadores aritmeticos

---
+++ Python/logica-programacao/aula008_operadores_aritmeticos.py
@@ -0,0 +1,25 @@
+adicio = 10 + 10
+print('Adição', adicio)
+
+subtracao = 10 - 5
+print('Subtracao', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicacao', multiplicacao)
+
+divisao = 10 / 2.2
+print('Divisao', divisao)
+
+divisao_inteira = 10 // 2.2
+print('Divisao inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('Mudulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
