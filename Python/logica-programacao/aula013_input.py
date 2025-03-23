f39543eebb8954abbee2709c1571cd40cca609a0
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-23 15:56:53 -0300

    intro input

---
+++ Python/logica-programacao/aula013_input.py
@@ -0,0 +1,13 @@
+# nome = input('Qual é seu nome? ')
+# print(f'O seu {nome=}')
+
+# solicitando dado
+numero_1 = (input('Digite um número: '))
+numero_2 = (input('Digite utro números: '))
+
+# fazendo a coerçao dos tipos de dados de str para int
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+# mostrando resultado
+print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
