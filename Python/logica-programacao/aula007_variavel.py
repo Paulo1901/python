8393e878c6ee71e3c3547bb745cf10a4047b6da2
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-22 21:47:29 -0300

    variavel

---
+++ Python/logica-programacao/aula007_variavel.py
@@ -0,0 +1,20 @@
+# Variáveis são usadas para salvar algo na mémoria do computador
+# PEP8: inicie variável com letras minusculas, pode usar
+# numeros e underlines
+# o sinal de = é operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável)
+# uso: nome_variavel = expressão # snake_case é utilizado em python 
+
+nome_completo = 'Paulo Carvalho'
+soma_dois_mais_dois = 2 + 2
+int_um = int('1')
+# print(int_um, type(int_um))
+# print(soma_dois_mais_dois, nome_completo)
+
+nome = 'Paulo'
+idade = 35
+maior_de_idade = idade >= 18
+print('Nome:',nome, 'idade: ',idade)
+print('É maior de idade: ', maior_de_idade)
+
+
