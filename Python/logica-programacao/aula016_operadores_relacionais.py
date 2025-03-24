0bbfe2a908a5707c4ebf700bd292aaeea9c1a7cb
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-24 11:34:07 -0300

    operadores relacionais

---
+++ Python/logica-programacao/aula016_operadores_relacionais.py
@@ -0,0 +1,18 @@
+# Operadores relacionais 
+""""
+> - maior
+< - menor
+>= - maior ou igual
+<= - menor ou igual
+== - igual
+!= - diferente
+"""
+
+maior = 2 > 1
+maior_igual = 2 >= 2
+menor = 1 < 2
+menor_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+
+print(maior)
