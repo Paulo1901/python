060d359bb659ab3c3331fbe2802a7b05f432362a
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-25 14:00:26 -0300

    interpolação strings

---
+++ Python/logica-programacao/aula020_interpolacao_string.py
@@ -0,0 +1,11 @@
+# Interpolação basica de string
+# s - string
+# d e i - int
+# f - float
+# x e X - Hexadecimal (ABCDEF0123456789)
+
+nome = 'Paulo'
+preco = 1000.95897643
+variavel = '%s, o preco é R$%.2f' % (nome, preco)
+print(variavel)
+
