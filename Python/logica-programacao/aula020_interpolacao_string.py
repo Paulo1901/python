1e0fd842b386c9d28019d4dc4d27ee52a3cd60ea
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-25 14:04:38 -0300

    hexadecimal

--- Python/logica-programacao/aula020_interpolacao_string.py
+++ Python/logica-programacao/aula020_interpolacao_string.py
@@ -8,4 +8,5 @@ nome = 'Paulo'
 preco = 1000.95897643
 variavel = '%s, o preco é R$%.2f' % (nome, preco)
 print(variavel)
-
+# Transformando em Hexadecimal
+print('O Hexadecimal de %d é %08x' % (1500, 1500))
