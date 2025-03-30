94e40786a076a8c63229bd1ae931711fb89ba977
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-30 15:29:41 -0300

    while condições

---
+++ Python/logica-programacao/aula028_while_condicoes.py
@@ -0,0 +1,15 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+loop infinito -> quando um code não tem fim
+"""
+
+contador = 0
+
+while contador <= 10:
+    print(contador)
+    
+    contador = contador + 1
+
+print('Acabou!')
