9cff6c75f11f95ae70ee0e165ec2313985e78f11
Paulo1901 <paulo.car1901@hotmail.com>
2025-03-30 01:52:13 -0300

    solicitando horas

--- Python/exercicios/ex005_3_em_1.py
+++ Python/exercicios/ex005_3_em_1.py
@@ -19,6 +19,18 @@ Faça um programa que pergunte a hora ao usuário e, baseando-se no horario
 descrito, exiba a saudação apropriada ex:
 Bom dia 0-11, boa tarde 12-17 e boa noite 18-23
 """
+saudacao = input('Que horas são? ')
+
+saudacao_int = int(saudacao)
+
+if saudacao_int <= 11:
+    print('Bom dia.')
+elif saudacao_int >= 12 and saudacao_int <= 17:
+    print('Boa tarde')
+elif saudacao_int >= 18 and saudacao_int <= 23:
+    print('Boa noite')
+else:
+    print('Por favor digite uma hora valida.')
 
 """
 Faça um programa que peça o primeiro nome do usuário, se o nome tiver 4 letras ou menos
