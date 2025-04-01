df09051838ebe750fb48d26bb6c9f76481ac7338
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-01 12:10:36 -0300

    finalizado calculadora

--- Python/exercicios/ex007_calculadora.py
+++ Python/exercicios/ex007_calculadora.py
@@ -7,6 +7,9 @@ while True:
 
     numeros_validos = None
 
+    num_1_float = 0
+    num_2_float = 0
+
     try:
         num_1_float = float(numero_1)
         num_2_float = float(numero_2)
@@ -26,9 +29,27 @@ while True:
     if len(operador) > 1:
         print('Digite apenas 1 operador')
         continue
+    
+    print('Realizando sua conta.')
+    print('confira o resultado abaixo: ')
+    if operador == '+':
+        print(f'{num_1_float} + {num_2_float} =', num_1_float + num_2_float)
+
+    elif operador == '-':
+        print(f'{num_1_float} - {num_2_float} =', num_1_float - num_2_float)
+
+    elif operador == '/':
+        print(f'{num_1_float} / {num_2_float} =', num_1_float / num_2_float)
+
+    elif operador == '*':
+        print(f'{num_1_float} * {num_2_float} =', num_1_float * num_2_float)
+
+    else:
+        print('Nunca deveria chegar aqui!')
 
     ##################
     sair = input('Quer sair? [s]sim: ').lower().startswith('s')
     
     if sair is True:
         break
+    
