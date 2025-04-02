c3f33eadd99be2efc8733dc5f91232f60f30a0b5
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-02 10:30:19 -0300

    inicio do problema com while

---
+++ Python/exercicios/ex008_while_qual_letra.py
@@ -0,0 +1,14 @@
+frase =  'o Python é uma linguagem de programação '\
+        'multiparadigma. '\
+        'Python foi criado por Guido Van Russunm. '
+
+# Count() para contar qtd da str
+# print(frase.count('a'))
+
+i = 0
+while i < len(frase):
+    letra_atual = frase[i]
+
+    print(letra_atual)
+    i += 1
+
