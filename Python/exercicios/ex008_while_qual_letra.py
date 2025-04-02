656802b3724218716eb57315835f07187f96a8a2
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-02 11:00:03 -0300

    terminado conta letra

--- Python/exercicios/ex008_while_qual_letra.py
+++ Python/exercicios/ex008_while_qual_letra.py
@@ -6,9 +6,26 @@ frase =  'o Python é uma linguagem de programação '\
 # print(frase.count('a'))
 
 i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
 while i < len(frase):
     letra_atual = frase[i]
-    qtd_vezes_letra_apareceu = frase.count(letra_atual)
+    
+    if letra_atual == ' ':
+        i += 1
+        continue
 
-    print(letra_atual, qtd_vezes_letra_apareceu)
+    qtd_vezes_letras_apareceu_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes < qtd_vezes_letras_apareceu_atual:
+        qtd_apareceu_mais_vezes = qtd_vezes_letras_apareceu_atual
+        letra_apareceu_mais_vezes = letra_atual
+    
     i += 1
+
+print(''
+    'A letra que apareceu mais vezes foi ' 
+    f'"{letra_apareceu_mais_vezes}" que apareceu '
+    f'{qtd_apareceu_mais_vezes}x '
+)
