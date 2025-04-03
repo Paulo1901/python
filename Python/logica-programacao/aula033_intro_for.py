b21b7ae59c5b937c3ed59133dc8fd85c3af8f49a
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-03 13:28:51 -0300

    init do for rabge

---
+++ Python/logica-programacao/aula033_intro_for.py
@@ -0,0 +1,14 @@
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+
+# numeros = range(0, 11, 1)
+# numeros = range(10, 0, -1) 
+# numeros = range(0, -11, -1) 
+numeros = range(0, 100, 2) 
+numeros = range(0, 100, 8) 
+
+
+for numero in numeros:
+    print(numero)
