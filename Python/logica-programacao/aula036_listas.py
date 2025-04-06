3b3d8bd5ca61ec5a5e697f1b3935944507329ea2
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-06 15:15:55 -0300

    tipos de lista

---
+++ Python/logica-programacao/aula036_listas.py
@@ -0,0 +1,24 @@
+"""
+Listas em Python
+Tipo list - mutável
+suporta vários valores e qualquer tipo
+conhecimento reutilizável - indices e fatiamento
+metodos úteis: append, insert pop, del, clear, extend, +
+"""
+
+#         01234
+#        -54321
+string = 'ABCDE' # 5 caracteres (len)
+
+# lista = list() - Umas das formas de se criar uma lista
+
+# # Criando uma lista com couchetes []
+
+# lista = [] # criando uma lista vazia
+
+# print(type(lista)) # type lista
+# print(bool(lista)) # Listas vazia é falsy
+
+# Listas são de tupo mutável, podendo ser mutável
+lista = [123, True, 'Paulo', 1.2, [456, 'Andreia']]
+print(lista[0])
