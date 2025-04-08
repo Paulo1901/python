d28e78efd4cddbf12bd09d10167ced193f2747a8
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-08 15:50:41 -0300

    intro desempacotamento

---
+++ Python/logica-programacao/aula042_empacotamento_desempacotamento.py
@@ -0,0 +1,5 @@
+# Introdução a empacotamento e desempacotamento
+
+# nomes = ['Paulo', 'Andreia', 'Davi'] # está empacotado "sacola de nomes"
+_, nome2, *resto = ['Paulo', 'Andreia', 'Davi'] # nessa linha esta contecendo o desmpacotamento dos nomes
+print(nome2)
