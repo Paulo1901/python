9f1628973d8cc0af8e81f4322b082ef12f17c382
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-08 23:06:30 -0300

    esclha as opções

--- Python/exercicios/ex012_lista_compra.py
+++ Python/exercicios/ex012_lista_compra.py
@@ -5,7 +5,17 @@ inserir, apagar e listar valores da sua lista
 nao permita que o programa quebre com
 erro de indices inexistentes na lista
 """
-lista = []
+lista = [] # criando uma lista vazia
 while True:
+    # interação com usuário
     print('Selecione uma opção')
     opcao = input('[i]inserir [a]apagar [l]lista ')
+
+    if opcao == 'i':
+        print('i')
+    elif opcao == 'a':
+        print('a')
+    elif opcao == 'l':
+        print('l')
+    else:
+        print('Por favor escolha i, a ou l')
