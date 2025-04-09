e51d6d2ded3e3e54d5d10ab2d8035fc7e5febd26
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-08 22:51:33 -0300

    enumerate

---
+++ Python/logica-programacao/aula044_enumerate.py
@@ -0,0 +1,24 @@
+"""
+Exercicios
+Exiba os índices da lista
+
+"""
+
+lista = ['Paulo', 'Andreia', 'Davi']
+lista.append('Maria')
+
+# lista_enumerada = list(enumerate(lista))
+
+# for item in enumerate(lista):
+#     indice, nome = item desempacotamento
+#     print(indice, nome)
+
+# forma melhor de fazer
+# for indice, nome in enumerate(lista):
+#     print(indice, nome)
+
+# for tupla_enumerada in enumerate(lista):
+#     print('For da tupla')
+#     for valor in tupla_enumerada:
+#         print(f'\t{valor}')
+
