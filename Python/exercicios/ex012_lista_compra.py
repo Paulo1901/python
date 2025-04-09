9b25d7974996ddc203c0c76403a922f8b9aac636
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-08 23:02:21 -0300

    init interação com usuario

---
+++ Python/exercicios/ex012_lista_compra.py
@@ -0,0 +1,11 @@
+"""
+Faça um lista de compras com listas
+o usuário deve ter a possibilidade de 
+inserir, apagar e listar valores da sua lista
+nao permita que o programa quebre com
+erro de indices inexistentes na lista
+"""
+lista = []
+while True:
+    print('Selecione uma opção')
+    opcao = input('[i]inserir [a]apagar [l]lista ')
