5f722b253d7ec7e7ed76a42fd7d6903814e97c62
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-08 22:33:34 -0300

    intro tuplas

---
+++ Python/logica-programacao/aula043_tuplas.py
@@ -0,0 +1,16 @@
+"""
+Tipo tuplas - uma lista imtável
+"""
+
+# Exemplo de lista 
+# Lista é um pouco mais lenta que tupla
+# nomes = ['Paulo', 'Andreia', 'Davi']
+# _, _, nome = nomes
+# print(nomes)
+# print(nome)
+
+# criando uma tupla = nomes = ('Paulo', 'Andreis', 'Davi')
+nomes = 'Paulo', 'Andreis', 'Davi'
+print(type(nomes))
+print(nomes[1])
+print(nomes)
