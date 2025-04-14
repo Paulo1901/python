468af06d7427811f2a9fa4096fedc48f6a193159
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-14 00:09:57 -0300

    concluido o exercicios do cpf

--- Python/exercicios/ex013_gerar_cpf.py
+++ Python/exercicios/ex013_gerar_cpf.py
@@ -23,16 +23,32 @@ contrario disso:
 
 o primeiro digito do CPF é 7
 """
-cpf = '74682489070'
-nove_digito = cpf[:9] # pegando os numeros primeiros digitos do cpf
+cpf_enviado_usuario = '39232116863'
+nove_digito = cpf_enviado_usuario[:9] # pegando os numeros primeiros digitos do cpf
 
 # criando um contador regressivo
 contador_regressivo_1 = 10
 
 resultado_digito_1 = 0
-for digito_1 in nove_digito:
-    resultado_digito_1 += int(digito_1) * contador_regressivo_1 
+for digito in nove_digito:
+    resultado_digito_1 += int(digito) * contador_regressivo_1 
     contador_regressivo_1 -= 1
 digito_1 = (resultado_digito_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
-print(digito_1)
+
+dez_digitos = nove_digito + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito_2 = 0
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2) * 10 % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_gerado_pelo_calculo = f'{nove_digito}{digito_1}{digito_2}'
+
+if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_enviado_usuario} é válido.')
+else:
+    print('CPF inválido')
