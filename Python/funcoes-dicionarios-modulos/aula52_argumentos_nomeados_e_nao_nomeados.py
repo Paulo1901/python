966f624f95431047b37c5801960a6898c7043f6f
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-15 01:02:05 -0300

    funções argumento posicional

---
+++ Python/funcoes-dicionarios-modulos/aula52_argumentos_nomeados_e_nao_nomeados.py
@@ -0,0 +1,12 @@
+"""
+Argumentos nomeados e não nomeados em funções Python
+Argumentos nomeados tem nome com sinal de igual
+Argumentos não nomeados recebe apenas o argumento (valor)
+"""
+
+# Definindo a função
+def soma(y, x):
+    print(f'{x=} + {y=}', x + y)
+
+# Executandoa função
+soma(2, 2) # argumento posicional
