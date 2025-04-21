ba147af47a776d8f91cd005ec53ff8c0d40334ad
Paulo1901 <paulo.car1901@hotmail.com>
2025-04-21 12:05:45 -0300

    parte 2 *args

--- Python/funcoes-dicionarios-modulos/aula56_args_parte1.py
+++ Python/funcoes-dicionarios-modulos/aula56_args_parte1.py
@@ -4,8 +4,8 @@ args - argumentos não nomeados
 """
 
 # Lembre-te de desempacotamento
-x, y, *resto = 1, 2, 3, 4
-print(x, y, resto)
+# x, y, *resto = 1, 2, 3, 4
+# print(x, y, resto)
 
 # def soma(x, y):
 #     return x + y
@@ -13,9 +13,20 @@ print(x, y, resto)
 def soma(*args):
     total = 0
     for numero in args:
-        print('Total', total, numero)
         total += numero
-        print('Total', total)
-    print(total)
+    return total
 
-soma(1,2,3,4,5,6)
+# soma_1_2_3 = soma(1,2,3)
+# print(soma_1_2_3)
+
+soma_1_2_3 = soma(1, 2, 3)
+# print(soma_1_2_3)
+
+soma_4_5_6 = soma(4, 5, 6)
+# print(soma_4_5_6)
+
+numeros = 1, 2, 3, 4, 5, 6, 7, 78, 10
+outra_soma = soma(*numeros)
+print(outra_soma)
+
+print(*numeros)
